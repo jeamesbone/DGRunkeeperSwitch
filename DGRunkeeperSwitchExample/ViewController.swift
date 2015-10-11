3a2dc99580a9d2adcb1b6f95fb5e6e26f675011d
Jeames Bone <jeames.bone@gmail.com>
2015-10-11 13:01:58 +1100

    Update example view controller with new construction methods

--- DGRunkeeperSwitchExample/ViewController.swift
+++ DGRunkeeperSwitchExample/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     // MARK: -
     // MARK: Vars
     
-    @IBOutlet weak var runkeeperSwitch4: DGRunkeeperSwitch?
+    @IBOutlet weak var runkeeperSwitch4: DGRunkeeperSwitch!
 
     // MARK: -
     // MARK: Lifecycle
@@ -26,19 +26,18 @@ class ViewController: UIViewController {
         navigationController!.navigationBar.translucent = false
         navigationController!.navigationBar.barTintColor = UIColor(red: 252.0/255.0, green: 182.0/255.0, blue: 54.0/255.0, alpha: 1.0)
         
-        let runkeeperSwitch = DGRunkeeperSwitch(leftTitle: "Feed", rightTitle: "Leaderboard")
+        let runkeeperSwitch = DGRunkeeperSwitch(titles: ["Feed", "Leaderboard"])
         runkeeperSwitch.backgroundColor = UIColor(red: 229.0/255.0, green: 163.0/255.0, blue: 48.0/255.0, alpha: 1.0)
         runkeeperSwitch.selectedBackgroundColor = .whiteColor()
         runkeeperSwitch.titleColor = .whiteColor()
         runkeeperSwitch.selectedTitleColor = UIColor(red: 255.0/255.0, green: 196.0/255.0, blue: 92.0/255.0, alpha: 1.0)
         runkeeperSwitch.titleFont = UIFont(name: "HelveticaNeue-Medium", size: 13.0)
-        runkeeperSwitch.frame = CGRect(x: 30.0, y: 40.0, width: 200.0, height: 30.0)
+        runkeeperSwitch.frame = CGRect(x: 30.0, y: 40.0, width: 300.0, height: 30.0)
         runkeeperSwitch.addTarget(self, action: Selector("switchValueDidChange:"), forControlEvents: .ValueChanged)
         navigationItem.titleView = runkeeperSwitch
         
         let runkeeperSwitch2 = DGRunkeeperSwitch()
-        runkeeperSwitch2.leftTitle = "Weekly"
-        runkeeperSwitch2.rightTitle = "Monthly"
+        runkeeperSwitch2.titles = ["test1", "test2", "test3"]
         runkeeperSwitch2.backgroundColor = UIColor(red: 239.0/255.0, green: 95.0/255.0, blue: 49.0/255.0, alpha: 1.0)
         runkeeperSwitch2.selectedBackgroundColor = .whiteColor()
         runkeeperSwitch2.titleColor = .whiteColor()
@@ -48,9 +47,7 @@ class ViewController: UIViewController {
         runkeeperSwitch2.autoresizingMask = [.FlexibleWidth]
         view.addSubview(runkeeperSwitch2)
         
-        let runkeeperSwitch3 = DGRunkeeperSwitch()
-        runkeeperSwitch3.leftTitle = "Super long left title"
-        runkeeperSwitch3.rightTitle = "Super long right title"
+        let runkeeperSwitch3 = DGRunkeeperSwitch(leftTitle: "Super long left title", rightTitle: "Super long right title")
         runkeeperSwitch3.backgroundColor = UIColor(red: 239.0/255.0, green: 95.0/255.0, blue: 49.0/255.0, alpha: 1.0)
         runkeeperSwitch3.selectedBackgroundColor = .whiteColor()
         runkeeperSwitch3.titleColor = .whiteColor()
@@ -59,16 +56,13 @@ class ViewController: UIViewController {
         runkeeperSwitch3.frame = CGRect(x: 50.0, y: 70.0, width: view.bounds.width - 100.0, height: 30.0)
         runkeeperSwitch3.autoresizingMask = [.FlexibleWidth]
         view.addSubview(runkeeperSwitch3)
-        
-        if let runkeeperSwitch4 = runkeeperSwitch4 {
-            runkeeperSwitch4.leftTitle = "Apple"
-            runkeeperSwitch4.rightTitle = "Google"
-            runkeeperSwitch4.backgroundColor = UIColor(red: 122/255.0, green: 203/255.0, blue: 108/255.0, alpha: 1.0)
-            runkeeperSwitch4.selectedBackgroundColor = .whiteColor()
-            runkeeperSwitch4.titleColor = .whiteColor()
-            runkeeperSwitch4.selectedTitleColor = UIColor(red: 135/255.0, green: 227/255.0, blue: 120/255.0, alpha: 1.0)
-            runkeeperSwitch4.titleFont = UIFont(name: "HelveticaNeue-Light", size: 17.0)
-        }
+
+        runkeeperSwitch4.titles = ["Apple", "Google"]
+        runkeeperSwitch4.backgroundColor = UIColor(red: 122/255.0, green: 203/255.0, blue: 108/255.0, alpha: 1.0)
+        runkeeperSwitch4.selectedBackgroundColor = .whiteColor()
+        runkeeperSwitch4.titleColor = .whiteColor()
+        runkeeperSwitch4.selectedTitleColor = UIColor(red: 135/255.0, green: 227/255.0, blue: 120/255.0, alpha: 1.0)
+        runkeeperSwitch4.titleFont = UIFont(name: "HelveticaNeue-Light", size: 17.0)
     }
     
     // MARK: -
